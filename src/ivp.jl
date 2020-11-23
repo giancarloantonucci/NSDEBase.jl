39548cc83f664d1347e0d38aeed3f4dcd0096e82
Giancarlo A. Antonucci <antonuccig@icloud.com>
2020-11-23 20:06:24 +0000

    ...

--- src/ivp.jl
+++ src/ivp.jl
@@ -13,7 +13,8 @@ struct InitialValueProblem{rhs_T, u0_T, tspan_T} <: NSDEProblem
     tspan::tspan_T
 end
 
+InitialValueProblem(f::Function, u0, tspan) = InitialValueProblem(RHS(f), u0, tspan)
 InitialValueProblem(rhs::RightHandSideFunction, u0::Number, tspan) = InitialValueProblem(rhs, [u0], tspan)
+InitialValueProblem(f::Function, u0, t0, tN) = InitialValueProblem(RHS(f), u0, t0, tN)
 InitialValueProblem(rhs::RightHandSideFunction, u0, t0::Real, tN::Real) = InitialValueProblem(rhs, u0, (t0, tN))
-InitialValueProblem(f::Function, args...; kwargs...) = InitialValueProblem(RHS(f), args...; kwargs...)
 @doc (@doc InitialValueProblem) IVP(args...; kwargs...) = InitialValueProblem(args...; kwargs...)
