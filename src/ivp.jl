6c9c4860087a9a139dd24c3f1e35faa5ccf98d3b
Giancarlo A. Antonucci <antonuccig@icloud.com>
2020-11-23 19:40:58 +0000

    Removed `θ`

--- src/ivp.jl
+++ src/ivp.jl
@@ -13,6 +13,7 @@ struct InitialValueProblem{rhs_T, u0_T, tspan_T} <: NSDEProblem
     tspan::tspan_T
 end
 
-InitialValueProblem(rhs, u0::Number, tspan) = InitialValueProblem(rhs, [u0], tspan)
-InitialValueProblem(rhs, u0, t0::Real, tN::Real) = InitialValueProblem(rhs, u0, (t0, tN))
+InitialValueProblem(rhs::RightHandSideFunction, u0::Number, tspan) = InitialValueProblem(rhs, [u0], tspan)
+InitialValueProblem(rhs::RightHandSideFunction, u0, t0::Real, tN::Real) = InitialValueProblem(rhs, u0, (t0, tN))
+InitialValueProblem(f::Function, args...; kwargs...) = InitialValueProblem(RHS(f), args...; kwargs...)
 @doc (@doc InitialValueProblem) IVP(args...; kwargs...) = InitialValueProblem(args...; kwargs...)
