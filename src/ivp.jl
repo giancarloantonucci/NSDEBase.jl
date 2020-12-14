79d1b8fc58d0471493d5cc07190ab93cacd29152
Giancarlo A. Antonucci <antonuccig@icloud.com>
2020-12-14 23:24:03 +0000

    ...

--- src/ivp.jl
+++ src/ivp.jl
@@ -14,8 +14,9 @@ mutable struct InitialValueProblem{rhs_T, u0_T, tspan_T} <: NSDEProblem
 end
 
 InitialValueProblem(rhs::RightHandSideFunction, u0::Number, tspan) = InitialValueProblem(rhs, [u0], tspan)
+InitialValueProblem(f::Function, u0, tspan) = InitialValueProblem(RHS(f), u0, tspan)
 InitialValueProblem(rhs::RightHandSideFunction, u0, t0::Real, tN::Real) = InitialValueProblem(rhs, u0, (t0, tN))
-InitialValueProblem(f::Function, args...; kwargs...) = InitialValueProblem(RHS(f), args...; kwargs...)
+InitialValueProblem(f::Function, u0, t0, tN) = InitialValueProblem(RHS(f), u0, t0, tN)
 @doc (@doc InitialValueProblem) IVP(args...; kwargs...) = InitialValueProblem(args...; kwargs...)
 
 function Base.copy(problem::InitialValueProblem)
