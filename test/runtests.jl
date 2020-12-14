d7e04a40fb328df1a08fb4573a4cfd3d496983ab
Giancarlo A. Antonucci <antonuccig@icloud.com>
2020-12-14 23:24:17 +0000

    Changed tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,32 +1,24 @@
 using NSDEBase
 using Test
 
-@testset "RHS and IVP" begin
+@testset "IVP and RHS" begin
     f(u, t) = @. u * (1.0 - u)
-    problem = IVP(f, 0.5, 0.0, 1.0)
+    problem = IVP(f, 0.5, (0.0, 1.0))
     @test problem isa InitialValueProblem
-    NSDEBase.@↓ rhs, u0, tspan = problem
-    @test rhs isa RightHandSideFunction
-    NSDEBase.@↓ f, f!, Df, Df! = rhs
-    @test typeof(rhs.f) <: Function
-    @test typeof(rhs.f!) <: Function
-    @test typeof(rhs.Df) <: Function
-    @test typeof(rhs.Df!) <: Function
+    @test problem.rhs isa RightHandSideFunction
+
+    f!(du, u, t) = @. du = u * (1.0 - u)
+    problem = IVP(f!, [0.5], 0.0, 1.0)
+    @test problem isa InitialValueProblem
+    @test problem.rhs isa RightHandSideFunction
 end
 
 @testset "ODEs" begin
-    problem = Dahlquist()
-    @test problem isa InitialValueProblem
-    problem = Logistic()
-    @test problem isa InitialValueProblem
-    problem = Riccati()
-    @test problem isa InitialValueProblem
-    problem = SimplePendulum()
-    @test problem isa InitialValueProblem
-    problem = VanderPol()
-    @test problem isa InitialValueProblem
-    problem = Lorenz()
-    @test problem isa InitialValueProblem
-    problem = Rössler()
-    @test problem isa InitialValueProblem
+    @test Dahlquist() isa InitialValueProblem
+    @test Logistic() isa InitialValueProblem
+    @test Riccati() isa InitialValueProblem
+    @test SimplePendulum() isa InitialValueProblem
+    @test VanderPol() isa InitialValueProblem
+    @test Lorenz() isa InitialValueProblem
+    @test Rössler() isa InitialValueProblem
 end
