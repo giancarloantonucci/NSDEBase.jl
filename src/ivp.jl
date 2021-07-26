7edfeadb49d0273bc52e27c298b1823a3ab5491e
Giancarlo Antonucci <giancarlo.antonucci@icloud.com>
2021-07-26 15:04:11 +0100

    Change back to `u0`, etc.

--- src/ivp.jl
+++ src/ivp.jl
@@ -1,28 +1,28 @@
 @doc """
-    InitialValueProblem(rhs, u₀, tspan) <: NSDEProblem
-    InitialValueProblem(rhs, u₀, t₀::Real, T::Real) <: NSDEProblem
+    InitialValueProblem(rhs, u0, tspan) <: NSDEProblem
+    InitialValueProblem(rhs, u0, t0::Real, tN::Real) <: NSDEProblem
     IVP(args...; kwargs...) <: NSDEProblem
 
 returns a constructor for an initial value problem.
 
 # Arguments
 - `rhs   :: Union{Function, RightHandSideFunction}` : right-hand side derivative.
-- `u₀    :: Union{Number, AbstractVector{Number}}`  : initial condition.
+- `u0    :: Union{Number, AbstractVector{Number}}`  : initial condition.
 - `tspan :: Tuple{Real, Real}`                      : time domain.
 """
-struct InitialValueProblem{rhs_T, u₀_T, tspan_T} <: NSDEProblem
+struct InitialValueProblem{rhs_T, u0_T, tspan_T} <: NSDEProblem
     rhs::rhs_T
-    u₀::u₀_T
+    u0::u0_T
     tspan::tspan_T
 end
 
-InitialValueProblem(rhs::RightHandSideFunction, u₀::Number, tspan) = InitialValueProblem(rhs, [u₀], tspan)
-InitialValueProblem(f::Function, u₀, tspan) = InitialValueProblem(RHS(f), u₀, tspan)
-InitialValueProblem(rhs::RightHandSideFunction, u₀, t₀::Real, T::Real) = InitialValueProblem(rhs, u₀, (t₀, T))
-InitialValueProblem(f::Function, u₀, t₀, T) = InitialValueProblem(RHS(f), u₀, t₀, T)
+InitialValueProblem(rhs::RightHandSideFunction, u0::Number, tspan) = InitialValueProblem(rhs, [u0], tspan)
+InitialValueProblem(f::Function, u0, tspan) = InitialValueProblem(RHS(f), u0, tspan)
+InitialValueProblem(rhs::RightHandSideFunction, u0, t0::Real, tN::Real) = InitialValueProblem(rhs, u0, (t0, tN))
+InitialValueProblem(f::Function, u0, t0, tN) = InitialValueProblem(RHS(f), u0, t0, tN)
 @doc (@doc InitialValueProblem) IVP(args...; kwargs...) = InitialValueProblem(args...; kwargs...)
 
 function Base.copy(problem::InitialValueProblem)
-    @↓ rhs, u₀, tspan = problem
-    return IVP(rhs, u₀, tspan)
+    @↓ rhs, u0, tspan = problem
+    return IVP(rhs, u0, tspan)
 end
