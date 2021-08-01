501e189416da005e512251cd9050d580338fde95
Giancarlo Antonucci <giancarlo.antonucci@icloud.com>
2021-08-01 21:33:48 +0100

    ...

--- src/ivp.jl
+++ src/ivp.jl
@@ -2,22 +2,27 @@
     InitialValueProblem{rhs_T, u0_T, tspan_T} <: NSDEProblem
 
 defines a constructor for an initial value problem.
+
+$(TYPEDFIELDS)
 """
 struct InitialValueProblem{rhs_T, u0_T, tspan_T} <: NSDEProblem
+    "right-hand side derivative"
     rhs::rhs_T
+    "initial condition"
     u0::u0_T
+    "time domain"
     tspan::tspan_T
 end
 
-"""
-    InitialValueProblem(rhs, u0, tspan) :: InitialValueProblem
-    IVP(args...; kwargs...) :: InitialValueProblem
-
-returns an `InitialValueProblem` from:
-- `rhs   :: Union{Function, RightHandSideFunction}` : right-hand side derivative.
-- `u0    :: Union{Number, AbstractVector{Number}}`  : initial condition.
-- `tspan :: Tuple{Real, Real}`                      : time domain.
-"""
+# """
+#     InitialValueProblem(rhs, u0, tspan) :: InitialValueProblem
+#     IVP(args...; kwargs...) :: InitialValueProblem
+#
+# returns an `InitialValueProblem` from:
+# - `rhs   :: Union{Function, RightHandSideFunction}` : right-hand side derivative.
+# - `u0    :: Union{Number, AbstractVector{Number}}`  : initial condition.
+# - `tspan :: Tuple{Real, Real}`                      : time domain.
+# """
 InitialValueProblem(rhs::RightHandSideFunction, u0::Number, tspan) = InitialValueProblem(rhs, [u0], tspan)
 InitialValueProblem(f::Function, u0, tspan) = InitialValueProblem(RHS(f), u0, tspan)
 
