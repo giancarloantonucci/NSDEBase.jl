53d65b6608af4b279edb3c5c0249539ec4c20ca1
Documenter.jl <documenter@juliadocs.github.io>
2021-08-01 20:42:03 +0000

    build based on 5c7cb00

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#NSDEBase.jl","page":"Home","title":"NSDEBase.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for NSDEBase.jl","category":"page"},{"location":"","page":"Home","title":"Home","text":"InitialValueProblem","category":"page"},{"location":"#NSDEBase.InitialValueProblem","page":"Home","title":"NSDEBase.InitialValueProblem","text":"InitialValueProblem{rhs_T, u0_T, tspan_T} <: NSDEProblem\n\ndefines a constructor for an initial value problem.\n\n\n\n\n\n","category":"type"}]
+[{"location":"#NSDEBase.jl","page":"Home","title":"NSDEBase.jl","text":"","category":"section"},{"location":"#API","page":"Home","title":"API","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Pages = [\"ivp.jl\"]","category":"page"}]
 }
