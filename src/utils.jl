c7d0fb32ccaebd0e332010c503a50990a91a20a0
Giancarlo Antonucci <giancarlo.antonucci@icloud.com>
2021-08-03 22:47:02 +0100

    Added max lvl to printing

--- src/utils.jl
+++ src/utils.jl
@@ -4,13 +4,20 @@ function solve! end
 function _show(io::IO, object)
     print(io, nameof(typeof(object)), ":")
     pad = get(io, :pad, "")
+    lvl = get(io, :lvl, 1)
     names = propertynames(object)
     N = length(names)
     for (n, name) in enumerate(names)
+        lvl_ = lvl
         field = getproperty(object, name)
         if field !== nothing
             print(io, "\n", pad, "   ‣ " * string(name) * " ≔ ")
-            show(IOContext(io, :pad => string(pad, "   ")), field)
+            if lvl < 2
+                lvl_ += 1
+                show(IOContext(io, :pad => string(pad, "   "), :lvl => lvl_), field)
+            else
+                summary(io, field)
+            end
         end
     end
 end
