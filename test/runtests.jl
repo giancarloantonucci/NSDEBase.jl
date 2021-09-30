e8ad68adb83576a28b5bad52abd6cf1febd01c31
Giancarlo Antonucci <giancarlo.antonucci@icloud.com>
2021-09-30 18:35:46 +0100

    Add tests with `showable`

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,7 +5,9 @@ using Test
     f(u, t) = @. u * (1.0 - u)
     problem = IVP(f, 0.5, (0.0, 1.0))
     @test problem isa InitialValueProblem
+    @test showable(MIME("text/plain"), problem)
     @test problem.rhs isa RightHandSideFunction
+    @test showable(MIME("text/plain"), problem.rhs)
 
     f!(du, u, t) = @. du = u * (1.0 - u)
     problem = IVP(f!, [0.5], 0.0, 1.0)
