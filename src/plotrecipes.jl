01735366d180c88b62c6c98c270cc71551296b4c
Giancarlo Antonucci <giancarlo.antonucci@icloud.com>
2022-02-01 01:04:53 +0000

    Add structured plotrecipe mechanism

--- src/plotrecipes.jl
+++ src/plotrecipes.jl
@@ -1,7 +1,17 @@
+# phaseplot
+mutable struct _PhasePlot{object_T}
+    object::object_T
+end
 @userplot PHASEPLOT
 @recipe function f(h::PHASEPLOT)
-    @series begin
-        plotkind --> :phaseplot
-        h.args[1]
-    end
+    return _PhasePlot(h.args[1])
+end
+
+# convergence
+mutable struct _Convergence{object_T}
+    object::object_T
+end
+@userplot CONVERGENCE
+@recipe function f(h::CONVERGENCE)
+    return _Convergence(h.args[1])
 end
