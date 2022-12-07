ca2099c08b85655649cdde3357d074088e98d85e
giancarloantonucci <giancarlo.antonucci@icloud.com>
2022-12-07 21:53:16 +0000

    Remove commented codes

--- src/utils.jl
+++ src/utils.jl
@@ -1,19 +1,8 @@
-# struct NSDEVector{data_T, idxs_T} <: AbstractVector
-#     data::data_T
-#     idxs::idxs_T
-# end
-#
-# v = NSDEVector{u_T}(undef, n, d)
-# v[i] == @view v.data[(i-1)*d+1:i*d]
-# v[mask(i)] = mask(v, i) = v.data[i]
-# extract(v, i) = [v.data[(j-1)*d+i] for j = 1:n]
-#
-# v = NSDEVector{u_T}(undef, n₁, n₂, d)
-# v = NSDEVector{u_T}(undef, n₁, n₂, n₃, d)
-
+# ToDo: NSDEVector
 Vector{u_T}(undef, n, d) where u_T = Vector{u_T}[Vector{u_T}(undef, d) for i = 1:n]
 Vector{u_T}(undef, n₂, n₁, d) where u_T = Vector{Vector{u_T}}[Vector{u_T}(undef, n₁, d) for i = 1:n₂]
 
+# ToDo: zero!(v::NSDEVector)
 zero!(v::AbstractVector{<:Number}) = fill!(v, zero(eltype(v)))
 function zero!(v::AbstractVector{<:AbstractVector{<:Number}})
     for i in eachindex(v)
@@ -21,11 +10,3 @@ function zero!(v::AbstractVector{<:AbstractVector{<:Number}})
     end
     return v
 end
-
-# function norm(v::AbstractVector{<:AbstractVector{<:Number}})
-#     r = zero(eltype(v))
-#     for i in eachindex(v)
-#         r += norm(v[i])
-#     end
-#     return r
-# end
