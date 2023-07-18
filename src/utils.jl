f73fdf12ba29b50832270fd3415e1bb005875dae
giancarloantonucci <giancarlo.antonucci@icloud.com>
2023-07-18 19:32:46 +0200

    Esthetic: Changed comments

--- src/utils.jl
+++ src/utils.jl
@@ -1,8 +1,8 @@
-# ToDo: NSDEVector
+# ToDo: write NSDEVector
 Vector{u_T}(undef, N, d) where u_T = Vector{u_T}[Vector{u_T}(undef, d) for i = 1:N]
 Vector{u_T}(undef, N2, N1, d) where u_T = Vector{Vector{u_T}}[Vector{u_T}(undef, N1, d) for i = 1:N2]
 
-# ToDo: zero!(v::NSDEVector)
+# ToDo: change to zero!(v::NSDEVector)
 zero!(v::AbstractVector{<:Number}) = fill!(v, zero(eltype(v)))
 function zero!(v::AbstractVector{<:AbstractVector{<:Number}})
     for i in eachindex(v)
