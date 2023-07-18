cec82ed414ebc1f26af1476975fcb129b3819ab3
giancarloantonucci <giancarlo.antonucci@icloud.com>
2023-07-18 19:16:55 +0200

    Rewritten `show`

--- src/show.jl
+++ src/show.jl
@@ -3,32 +3,43 @@
 
 prints the full description of an `object` and its contents to a stream `io`.
 """
-function Base.show(io::IO, object::AbstractObject)
-    print(io, nameof(typeof(object)))
-    padding = get(io, :padding, "")
-    thislevel = get(io, :thislevel, 1)
-    namesoffields = propertynames(object)
-    numberofnames = length(namesoffields)
-    numberoffieldstoignore = 0
-    for n = numberofnames:-1:1
-        field = getproperty(object, namesoffields[n])
-        field isa Nothing ? numberoffieldstoignore += 1 : break
+function Base.show(io::IO, object::AbstractObject; depth=4)
+    summary(io, object)
+    names = propertynames(object)
+    num_properties = length(names)
+    nothing_properties = 0
+    for n = num_properties:-1:1
+        if getproperty(object, names[n]) isa Nothing
+            nothing_properties += 1
+        else
+            break
+        end
     end
-    numberofnames -= numberoffieldstoignore
-    for (n, nameoffield) in enumerate(namesoffields)
-        level = thislevel
-        field = getproperty(object, nameoffield)
-        if field !== nothing
-            print(io, "\n", padding, n < numberofnames ? "├─── " : "└─── ", string(nameoffield))
-            if thislevel < 5 && !(field isa AbstractArray)
-                level += 1
-                tmpio = IOContext(io, :padding => string(padding, n < numberofnames ? "│    " : "     "), :thislevel => level)
-                print(tmpio, " = ")
-                show(tmpio, field)
+    num_properties -= nothing_properties
+    padding = get(io, :padding, "")
+    current_level = get(io, :current_level, 1)
+    for (n, name) in enumerate(names)
+        property = getproperty(object, name)
+        if property isa Nothing
+            continue
+        end
+        is_property_last = n == num_properties
+        level = current_level
+        print(io, "\n", padding, (is_property_last ? "└─ " : "├─ "), string(name))
+        if current_level < depth
+            level += 1
+            io2 = IOContext(io, :padding => string(padding, is_property_last ? "   " : "│  "), :current_level => level)
+            print(io, " :: ")
+            summary(io, property)
+            if property isa Number || property isa AbstractArray
+                print(io, " = ")
+                show(IOContext(io2, :limit => true), property)
             else
-                print(io, " :: ")
-                summary(io, field)
+                show(io2, property)
             end
+        else
+            print(io, " :: ")
+            summary(io, property)
         end
     end
 end
