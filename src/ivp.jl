548e186a583c2a1f7e7e6619e559c3fed8419c84
Giancarlo A. Antonucci <giancarlo.antonucci@icloud.com>
2023-08-24 16:48:11 +0100

    Change spacing and add to-do reminder

--- src/ivp.jl
+++ src/ivp.jl
@@ -19,15 +19,16 @@ IVP(args...; kwargs...)
 - [`new`](@ref) : new instance of problem with updated parameters.
 """
 struct InitialValueProblem{
-    rhs_T <: AbstractRightHandSide,
-    u0_T <: AbstractVector{<:Number},
+    rhs_T   <: AbstractRightHandSide,
+    u0_T    <: AbstractVector{<:Number},
     tspan_T <: (Tuple{ℝ, ℝ} where ℝ <: Real),
     } <: AbstractInitialValueProblem
-    rhs :: rhs_T
-    u0 :: u0_T
+    rhs   :: rhs_T
+    u0    :: u0_T
     tspan :: tspan_T
 end
 
+# TODO: Add methods for SRHS
 InitialValueProblem(L::Union{ℂ, AbstractMatrix{ℂ}}, u0::AbstractVector{ℂ}, tspan::Tuple{ℝ, ℝ}) where {ℂ <: Number, ℝ <: Real} = InitialValueProblem(LRHS(L), u0, tspan)
 InitialValueProblem(f::Function, u0::AbstractVector{<:Number}, tspan::Tuple{ℝ, ℝ}) where ℝ <: Real = InitialValueProblem(RHS(f), u0, tspan)
 InitialValueProblem(rhs::Union{ℂ, AbstractMatrix{ℂ}, Function, AbstractRightHandSide}, u0::ℂ, tspan::Tuple{ℝ, ℝ}) where {ℂ <: Number, ℝ <: Real} = InitialValueProblem(rhs, [u0], tspan)
